8f5343fe50780302914d96f4e80f897e8d351916
Leonardo Cogo <leonardosima.cogo205@gmail.com>
2025-05-11 18:13:27 -0300

    feat: consumo de noticias da News API

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,28 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-import Header from "./components/Header.js";
+import Header from "./components/Header";
 import NewsSection from './components/NewsSection';
 
-export default function Home() {
+async function getNews() {
+  const apiKey = '304c10c7af58435186c359b04ce3b9e0';
+  const res = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`
+  );
+  const data = await res.json();
+
+  const filtered = data.articles?.filter(article => article.urlToImage) || [];
+  return filtered;
+}
+
+export default async function Home() {
+  const articles = await getNews();
+
   return (
     <div className={styles.page}>
       <header className={styles.headerContainer}>
         <Header />
       </header>
       <main className={styles.main}>
-        <NewsSection />
+        <NewsSection articles={articles} />
       </main>
     </div>
   );
